737f8f7ae28a5ede85c3c329ce99641d44bd4da8
Evans Chaun <evanschaun@gmail.com>
2024-01-20 11:45:37 +0300

    Push lesson 3

--- Lesson2DisplayText/Lesson2DisplayText/Lesson2DisplayText.py
+++ Lesson2DisplayText/Lesson2DisplayText/Lesson2DisplayText.py
@@ -1,1 +1,8 @@
+print("Rumuruti is the best Town in Laikipia")
+print('I " live " in rumuruti')
+print("It's a beiutiful town")
 
+print(" I was born, \n and rised up in Churo")
+
+print(""" I was born, \n and 
+rised up in Churo """)
