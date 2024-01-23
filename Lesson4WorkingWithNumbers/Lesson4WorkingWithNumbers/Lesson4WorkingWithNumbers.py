850eac1fc5548010537f9c5537afcb336e85ccd6
Evans Chaun <evanschaun@gmail.com>
2024-01-23 19:23:20 +0300

    Updates

--- Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers.py
+++ Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers.py
@@ -11,3 +11,11 @@ print(" The are of triangle will be %.f" % area )
 #printing the formated decimal number with right justified to take 6 spaces 
 #with leading 0s
 print("My favorite number is %06d !" % 42)
+
+# Do the same thing with the .format syntax to include out output
+print("I have {0:.2f} cats".format(area)) 
+print("I have {0:f} cats".format(area)) 
+print("I have {0:03d} cats".format(6)) 
+
+
+
---
+++ Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers/askFromUser.py
@@ -0,0 +1,7 @@
+salary = input("Please enter your salary: ")
+
+bonus= input("Please enter your bonus: ")
+payCheck = salary + bonus
+payCheck = float(salary) + float(bonus)
+print(payCheck)
+
