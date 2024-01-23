194ae614cb953a850f8097b49bc859440a7f78d1
Evans Chaun <evanschaun@gmail.com>
2024-01-23 15:09:15 +0300

    Updates

--- Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers.py
+++ Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers/Lesson4WorkingWithNumbers.py
@@ -6,3 +6,8 @@ area = height*width /2
 print(area)
 
 # Storing numbers
+
+print(" The are of triangle will be %.f" % area )
+#printing the formated decimal number with right justified to take 6 spaces 
+#with leading 0s
+print("My favorite number is %06d !" % 42)
