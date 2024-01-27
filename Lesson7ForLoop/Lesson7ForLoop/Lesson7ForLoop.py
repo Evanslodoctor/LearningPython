a2a840c7b6e3ead2b1367205b7ee8ba2fe675b3b
Evans Chaun <evanschaun@gmail.com>
2024-01-27 12:20:44 +0300

    Pushing lesson 7,

---
+++ Lesson7ForLoop/Lesson7ForLoop/Lesson7ForLoop.py
@@ -0,0 +1,12 @@
+import turtle
+
+TeamApic = ["Evans Chaun", "John Wambua", "Ashley Apondi", "Simon Ngaruiya", "Dina Anyango"]
+
+for names in TeamApic:
+    print(names)
+
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+turtle.done()
